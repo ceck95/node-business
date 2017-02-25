f1d759fb6f7e188dd345bab1512286debc6cfaff
nhutdev <tranvannhut4495@gmail.com>
2017-02-25 12:13:11 +0700

    fix validate fumc

--- authenticators/oauth-bearer-local-and-basic.js
+++ authenticators/oauth-bearer-local-and-basic.js
@@ -3,7 +3,7 @@
  * @Date:   2017-02-25T09:26:40+07:00
  * @Email:  tranvannhut4495@gmail.com
 * @Last modified by:   nhutdev
-* @Last modified time: 2017-02-25T11:43:31+07:00
+* @Last modified time: 2017-02-25T12:09:56+07:00
  */
 
 
@@ -52,14 +52,14 @@ class OAuthBearerLocalAuthenticator {
 
     switch (settings.tokenTypeCurrent) {
       case 'Bearer':
-        this.config.tokenType = 'Bearer';
-        let bearerLocal = new OAuthBearerLocal(this.config);
-        bearerLocal.validate(token, settings, callback);
+        settings.tokenType = 'Bearer';
+        let bearerLocal = new OAuthBearerLocal(settings);
+        bearerLocal.validate.apply(token, settings, callback);
         break;
       case 'Basic':
-        this.config.tokenType = 'Basic';
-        let basicLocal = new OAuthBasicLocal(this.config);
-        basicLocal.validate(token, settings, callback);
+        settings.tokenType = 'Basic';
+        let basicLocal = new OAuthBasicLocal(settings);
+        basicLocal.validate.apply(token, settings, callback);
         break;
     }
 
